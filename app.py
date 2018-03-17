74a4d3b92c534f64b0d3b024b48ff4082343c4f7
bkhadka89 <bkhadka89@gmail.com>
2018-03-17 12:49:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,84 +41,23 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res1 = processRequest_chen(req)
-
-    res 1= json.dumps(res1, indent=4)
-    # print(res)
-    r1 = make_response(res1)
-    r1.headers['Content-Type'] = 'application/json'
-    #########
     res = processRequest(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
-    return r , r1
-##############################
-def processRequest_chen(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
-        return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    res1 = "you are right"
-    return res1
-############################
+    return r
+
 
 def processRequest(req):
-    if req.get("result").get("action") != "yahooWeatherForecast":
+    if req.get("result").get("action") != "interest":
         return {}
-    baseurl = "https://query.yahooapis.com/v1/public/yql?"
-    yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
-    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
-    result = urlopen(yql_url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
-    return res
-
-
-def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
     city = parameters.get("geo-city")
-    if city is None:
-        return None
-
-    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
-
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    item = channel.get('item')
-    location = channel.get('location')
-    units = channel.get('units')
-    if (location is None) or (item is None) or (units is None):
-        return {}
-
-    condition = item.get('condition')
-    if condition is None:
-        return {}
-
-    # print(json.dumps(item, indent=4))
-
-    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
-             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
-
+    cost = {'Fairfax': '6.7 %','Fallschurch': '2.9%'}
+    speech = "Today the Interest rate in " + city + ": " + str(cost[city])
     print("Response:")
     print(speech)
 
