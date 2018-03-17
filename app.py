aaa0f6b078358083ccd3fed992d8098fb3e36198
bkhadka89 <bkhadka89@gmail.com>
2018-03-17 12:25:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,30 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+
+  More 
+1 of 312
+ 
+chen's 
+Inbox
+x 
+
+mingda chen
+Attachments12:22 PM (0 minutes ago)
+to me 
+
+Attachments area
+	
+Click here to Reply or Forward
+1.96 GB (13%) of 15 GB used
+Manage
+Terms - Privacy
+Last account activity: 0 minutes ago
+Details
+
+Executable File  122 lines (92 sloc)  3.19 KB
 # -*- coding:utf8 -*-
 # !/usr/bin/env python
 # Copyright 2017 Google Inc. All Rights Reserved.
@@ -39,35 +66,93 @@ def webhook():
 
     print("Request:")
     print(json.dumps(req, indent=4))
+
+    res1 = processRequest_chen(req)
+
+    res1= json.dumps(res1, indent=4)
+    # print(res)
+    r1 = make_response(res1)
+    r1.headers['Content-Type'] = 'application/json'
+    #########
     res = processRequest(req)
-   
-    
+
     res = json.dumps(res, indent=4)
-   
     # print(res)
     r = make_response(res)
- 
     r.headers['Content-Type'] = 'application/json'
-    return r
- 
-
-
-def processRequest(req):
+    return r , r1
+##############################
+def processRequest_chen(req):
     if req.get("result").get("action") != "topUniversity":
         return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    res1 = "you are right"
+    return res1
+############################
+
+def processRequest(req):
+    if req.get("result").get("action") != "yahooWeatherForecast":
+        return {}
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    yql_query = makeYqlQuery(req)
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
     result = req.get("result")
     parameters = result.get("parameters")
-    state=parameters.get("geo-state")
-    topUniversity={'Virginia':'Virgina Polytechnic Institue of State University'}
-    if topUniversity is None:
-        return "I am from webhook"
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
 
-    speech="According to the Best Enginnering College Website. Top 5 Best Enginnering College in :"+ state+ "are"+str(state[topUniversity])
-    print("Response: ")
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
     print(speech)
+
     return {
-        "speech":speech,
-        "displayText":speech,
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
 
