0270bc07029bc7dad48e2b8d62b91ab847e33cb0
bkhadka89 <bkhadka89@gmail.com>
2018-03-23 23:51:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
 
-import urllib
-import requests
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 from flask import Flask
 from flask import request
 from flask import make_response
+
 
 # Flask app should start in global layout
 app = Flask(__name__)
